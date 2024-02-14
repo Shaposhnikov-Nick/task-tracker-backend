fb6e58b2d2a8d4afe7cc2d6ec689c280aa0ca193
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-14 14:26:59 +0300

    init admin-service

---
+++ admin-service/src/main/kotlin/ru/taskrtacker/adminservice/AdminServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.taskrtacker.adminservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdminServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdminServiceApplication>(*args)
+}
