843fef5dea9647662ec50c9f80d0c999e5b50d5a
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-19 16:04:37 +0300

    fix package name

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/repository/TaskRepository.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/repository/TaskRepository.kt
@@ -1,7 +1,7 @@
-package ru.taskrtacker.tasktrackerservice.repository
+package ru.tasktracker.taskservice.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
-import ru.taskrtacker.tasktrackerservice.entity.Task
+import ru.tasktracker.taskservice.entity.Task
 
 
 interface TaskRepository : JpaRepository<Task, Long>
