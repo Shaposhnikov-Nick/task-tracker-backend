77891c8d7e164958c6b75050d114c233219f98c3
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-19 16:05:41 +0300

    fix package name

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/TaskTrackerServiceApplication.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/TaskTrackerServiceApplication.kt
@@ -1,4 +1,4 @@
-package ru.taskrtacker.tasktrackerservice
+package ru.tasktracker.taskservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
