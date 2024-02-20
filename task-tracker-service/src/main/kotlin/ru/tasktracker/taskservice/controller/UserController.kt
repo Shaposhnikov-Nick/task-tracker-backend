1545184af278c6b47e31608cfdd6b084c3b082d0
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-20 12:34:01 +0300

    API for update user profile

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/UserController.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/UserController.kt
@@ -1,21 +1,24 @@
 package ru.tasktracker.taskservice.controller
 
-import jakarta.validation.Valid
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.tasktracker.taskservice.dto.UserDto
+import ru.tasktracker.taskservice.dto.validation.ValidationGroups
 import ru.tasktracker.taskservice.service.UserService
 
 
 @Validated
 interface UserController {
 
-    fun createUser(@RequestBody @Valid userDto: UserDto): UserDto
+    fun createUser(@RequestBody @Validated(ValidationGroups.Create::class) userDto: UserDto): UserDto
+
+    fun updateUser(@RequestBody @Validated(ValidationGroups.Update::class) userDto: UserDto): UserDto
 
     fun getUser(@PathVariable id: Long): UserDto
 
@@ -30,6 +33,11 @@ class UserControllerImpl(
     @PostMapping
     override fun createUser(userDto: UserDto): UserDto {
         return userService.createUser(userDto)
+    }
+
+    @PutMapping
+    override fun updateUser(userDto: UserDto): UserDto {
+        return userService.updateUserProfile(userDto)
     }
 
     @GetMapping("/{id}")
