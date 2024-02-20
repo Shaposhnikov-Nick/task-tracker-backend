5359552f0fb8749043e111296a13c455289230e2
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-20 12:52:19 +0300

    change api for update user profile

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/service/UserService.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/service/UserService.kt
@@ -12,6 +12,10 @@ interface UserService {
     fun createUser(userDto: UserDto): UserDto
 
     fun getUser(id: Long): UserDto
+
+    fun updateUserProfile(userDto: UserDto): UserDto
+
+    fun changePassword(userDto: UserDto) : UserDto // TODO
 }
 
 @Service
@@ -29,6 +33,26 @@ class UserServiceImpl(
     @Transactional(readOnly = true)
     override fun getUser(id: Long): UserDto {
         return userRepository.findUserById(id)?.mapTo() ?: throw RuntimeException("User with id $id not found")
+    }
+
+    @Transactional
+    override fun updateUserProfile(userDto: UserDto): UserDto {
+        val user = userRepository.findUserById(userDto.id!!) ?: throw RuntimeException("User with id ${userDto.id} not found")
+
+        user.profile.about = userDto.profile.about
+        user.profile.name = userDto.profile.name
+        user.profile.lastName = userDto.profile.lastName
+        user.profile.birthday = userDto.profile.birthday
+        userDto.profile.email?.let {
+            user.profile.email = it
+            user.emailConfirmed = false
+        }
+
+         return userRepository.saveAndFlush(user).mapTo()
+    }
+
+    override fun changePassword(userDto: UserDto): UserDto {
+        TODO("Not yet implemented")
     }
 
 }
