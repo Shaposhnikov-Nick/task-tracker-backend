9a8ef09cb9027e6151b266e586f6baf64935f65c
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-26 17:35:05 +0300

    add audit config

---
+++ auth-service/src/main/kotlin/ru/tasktracker/authservice/config/AuditConfig.kt
@@ -0,0 +1,38 @@
+package ru.tasktracker.authservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
+
+
+@Configuration
+@EnableJpaAuditing
+class AuditConfig {
+
+    @Bean
+    fun auditorProvider(): AuditorAware<String> {
+        return SpringSecurityAuditorAware
+    }
+
+    companion object SpringSecurityAuditorAware : AuditorAware<String> {
+        override fun getCurrentAuditor(): Optional<String> {
+            val auth = SecurityContextHolder.getContext().authentication
+
+            return if (auth == null || !auth.isAuthenticated)
+                throw IllegalStateException("No user in SecurityContext!")
+            else {
+                val principal = auth.principal
+                if (principal is org.springframework.security.core.userdetails.User) {
+                    Optional.of(principal.username)
+                } else {
+                    Optional.of("none") // TODO: fix with real user
+                }
+
+            }
+        }
+    }
+
+}
