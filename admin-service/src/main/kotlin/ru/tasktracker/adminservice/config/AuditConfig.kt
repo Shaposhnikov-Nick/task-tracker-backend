eb4e1e8774abb960804c549ed291efcd37bb0fe9
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-29 12:37:42 +0300

    add audit config

---
+++ admin-service/src/main/kotlin/ru/tasktracker/adminservice/config/AuditConfig.kt
@@ -0,0 +1,37 @@
+package ru.tasktracker.adminservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.security.core.context.SecurityContextHolder
+import ru.tasktracker.adminservice.auth.AuthenticatedUser
+import java.util.*
+
+
+@Configuration
+@EnableJpaAuditing
+class AuditConfig {
+
+    @Bean
+    fun auditorProvider(): AuditorAware<String> {
+        return SpringSecurityAuditorAware
+    }
+
+    companion object SpringSecurityAuditorAware : AuditorAware<String> {
+        override fun getCurrentAuditor(): Optional<String> {
+            val auth = SecurityContextHolder.getContext().authentication
+
+            return if (auth != null && auth.isAuthenticated) {
+                val principal = auth.principal
+                if (principal is AuthenticatedUser) {
+                    Optional.of(principal.login)
+                } else {
+                    Optional.of("none")
+                }
+            } else {
+                Optional.of("none")
+            }
+        }
+    }
+}
