2fddc5e9bdc82b19991bb4277b82c98ba2544b86
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-02-29 12:38:16 +0300

    add security config

---
+++ admin-service/src/main/kotlin/ru/tasktracker/adminservice/config/SecurityConfig.kt
@@ -0,0 +1,47 @@
+package ru.tasktracker.adminservice.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import ru.tasktracker.adminservice.auth.JwtFilter
+import java.security.SecureRandom
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    val jwtFilter: JwtFilter
+) {
+
+    @Bean
+    fun jwtFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors(Customizer.withDefaults())
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { authorize ->
+                authorize
+                    .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
+                    .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger-ui/**")).permitAll()
+                    .requestMatchers(AntPathRequestMatcher.antMatcher("/actuator/*/**")).permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder(10, SecureRandom())
+}
