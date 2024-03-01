83960ea8c1af62aa410dc1151f15479d3db62f82
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-03-01 11:09:27 +0300

    add auth in task controller

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/TaskController.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/TaskController.kt
@@ -1,16 +1,18 @@
 package ru.tasktracker.taskservice.controller
 
 
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import ru.tasktracker.taskservice.auth.AuthenticatedUser
 import ru.tasktracker.taskservice.dto.TaskDto
 import ru.tasktracker.taskservice.service.TaskService
 
 
 interface TaskController {
-    fun getTasks(@PathVariable userId: Long): List<TaskDto>
+    fun getTasks(@AuthenticationPrincipal authUser: AuthenticatedUser): List<TaskDto>
 }
 
 
@@ -20,9 +22,10 @@ class TaskControllerImpl(
     val taskService: TaskService
 ) : TaskController {
 
-    @GetMapping("/{userId}")
-    override fun getTasks(userId: Long): List<TaskDto> {
-        return taskService.getAllTasks(userId)
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @GetMapping
+    override fun getTasks(authUser: AuthenticatedUser): List<TaskDto> {
+        return taskService.getAllTasks(authUser.id)
     }
 
 }
