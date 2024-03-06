bd72174dea20c685b328dcd442710aab644df68b
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-03-06 14:07:00 +0300

    [Task] add API for create task

--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/TaskController.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/controller/TaskController.kt
@@ -3,21 +3,41 @@ package ru.tasktracker.taskservice.controller
 
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.tasktracker.taskservice.auth.AuthenticatedUser
 import ru.tasktracker.taskservice.dto.TaskDto
+import ru.tasktracker.taskservice.dto.validation.ValidationGroups
 import ru.tasktracker.taskservice.service.TaskService
 
-
+@Validated
 interface TaskController {
     fun getTasks(@AuthenticationPrincipal authUser: AuthenticatedUser): List<TaskDto>
+
+    fun createTask(
+        @AuthenticationPrincipal authUser: AuthenticatedUser,
+        @RequestBody @Validated(ValidationGroups.Create::class) taskDto: TaskDto
+    ): List<TaskDto>
+
+    fun getTask(
+        @AuthenticationPrincipal authUser: AuthenticatedUser,
+        @PathVariable taskId: Long
+    ): TaskDto
+
+    fun updateTask(@AuthenticationPrincipal authUser: AuthenticatedUser): TaskDto
+    fun deleteTask(@AuthenticationPrincipal authUser: AuthenticatedUser): List<TaskDto>
+
+
 }
 
 
 @RestController
-@RequestMapping("task")
+@RequestMapping("tasks")
 class TaskControllerImpl(
     val taskService: TaskService
 ) : TaskController {
@@ -26,6 +46,30 @@ class TaskControllerImpl(
     @GetMapping
     override fun getTasks(authUser: AuthenticatedUser): List<TaskDto> {
         return taskService.getAllTasks(authUser.id)
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PostMapping
+    override fun createTask(authUser: AuthenticatedUser, taskDto: TaskDto): List<TaskDto> {
+        return taskService.createTask(authUser, taskDto)
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @GetMapping("/{taskId}")
+    override fun getTask(authUser: AuthenticatedUser, taskId: Long): TaskDto {
+        TODO("Not yet implemented")
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PostMapping
+    override fun updateTask(authUser: AuthenticatedUser): TaskDto {
+        TODO("Not yet implemented")
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @PostMapping
+    override fun deleteTask(authUser: AuthenticatedUser): List<TaskDto> {
+        TODO("Not yet implemented")
     }
 
 }
--- task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/service/TaskService.kt
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/service/TaskService.kt
@@ -2,27 +2,39 @@ package ru.tasktracker.taskservice.service
 
 import cz.encircled.skom.Extensions.mapTo
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import ru.tasktracker.taskservice.auth.AuthenticatedUser
 import ru.tasktracker.taskservice.dto.TaskDto
+import ru.tasktracker.taskservice.entity.Task
+import ru.tasktracker.taskservice.exception.UserException
 import ru.tasktracker.taskservice.repository.TaskRepository
+import ru.tasktracker.taskservice.repository.UserRepository
 
 interface TaskService {
 
-    fun addTask(task: TaskDto): TaskDto
-
     fun getAllTasks(userId: Long): List<TaskDto>
+
+    fun createTask(authUser: AuthenticatedUser, taskDto: TaskDto): List<TaskDto>
 
 }
 
 @Service
 class TaskServiceImpl(
-    val taskRepository: TaskRepository
+    val taskRepository: TaskRepository,
+    val userRepository: UserRepository
 ) : TaskService {
-    override fun addTask(task: TaskDto): TaskDto {
-        TODO("Not yet implemented")
-    }
 
+    @Transactional(readOnly = true)
     override fun getAllTasks(userId: Long): List<TaskDto> {
         return taskRepository.findAllByUserId(userId).mapTo()
+    }
+
+    @Transactional
+    override fun createTask(authUser: AuthenticatedUser, taskDto: TaskDto): List<TaskDto> {
+        val user = userRepository.findUserById(authUser.id)
+            ?: throw UserException("User with id ${authUser.id} not found")
+        user.addTask(taskDto.mapTo<Task>())
+        return userRepository.saveAndFlush(user).tasks.mapTo<TaskDto>().toList()
     }
 
 }
