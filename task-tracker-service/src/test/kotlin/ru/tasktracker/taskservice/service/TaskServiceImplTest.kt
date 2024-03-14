7c099ca08b0ca1d28bbd7598249012d58896248c
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2024-03-14 16:21:00 +0300

    [Task] integrations test for task service

--- task-tracker-service/src/test/kotlin/ru/tasktracker/taskservice/service/TaskServiceImplTest.kt
+++ task-tracker-service/src/test/kotlin/ru/tasktracker/taskservice/service/TaskServiceImplTest.kt
@@ -1,85 +1,38 @@
 package ru.tasktracker.taskservice.service
 
-import org.junit.jupiter.api.BeforeAll
+import cz.encircled.skom.Extensions.mapTo
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.junit.jupiter.Container
-import org.testcontainers.junit.jupiter.Testcontainers
-import org.testcontainers.utility.MountableFile
-import ru.tasktracker.taskservice.entity.*
-import ru.tasktracker.taskservice.repository.TaskRepository
-import ru.tasktracker.taskservice.repository.UserRepository
-import java.time.LocalDateTime
+import ru.tasktracker.taskservice.BaseTest
 
 
-@Testcontainers
 @SpringBootTest
-class TaskServiceImplTest {
-    @Autowired
-    lateinit var taskRepository: TaskRepository
+class TaskServiceImplTest : BaseTest() {
 
     @Autowired
-    lateinit var userRepository: UserRepository
-
-    lateinit var user: User
-
-    companion object {
-
-        @Container
-        @ServiceConnection
-        val container = PostgreSQLContainer("postgres:15")
-            .withCopyFileToContainer(MountableFile.forClasspathResource("init.sql"), "/docker-entrypoint-initdb.d/")
-
-        @JvmStatic
-        @BeforeAll
-        fun init() {
-            container.execInContainer("insert into users values (1,22222222,tests,false,false,1,2024-03-04 17:02:42.778983,2024-03-04 17:02:42.778983,none)")
-        }
-    }
-
-//    @BeforeEach
-//    fun init() {
-//        val newUser = User(
-//            "test",
-//            "test",
-//            false,
-//            false,
-//            mutableSetOf(),
-//            UserProfile(null, "name", "lastname", LocalDateTime.now(), "test@test.ru", null, null)
-//        )
-//        user = userRepository.saveAndFlush(newUser)
-//    }
+    lateinit var taskService: TaskService
 
     @Test
     fun getAllTasks() {
-        val tasks = taskRepository.findAllByUserId(1)
-        tasks.size
+        val tasks = taskService.getAllTasks(user.id!!)
+
+        assertThat(tasks)
+            .isNotNull()
+            .isNotEmpty()
+            .usingRecursiveFieldByFieldElementComparatorIgnoringFields(
+                "id",
+                "createdDate",
+                "updateDate",
+                "updatedBy",
+                "deadLine"
+            )
+            .contains(task.mapTo())
     }
 
     @Test
     fun createTask() {
-        val tasks = taskRepository.findAllByUserId(1)
-
-        val task = Task(
-            title = "Test title",
-            description = "Test Description",
-            status = TaskStatus.TODO,
-            priority = TaskPriority.LOW,
-            deadLine = LocalDateTime.now().plusDays(1),
-            assigneeId = 1,
-            group = null,
-            taskComments = mutableSetOf(),
-            parentId = null
-        )
-
-        taskRepository.saveAndFlush(task)
-
-        val tasks2 = taskRepository.findAllByUserId(1)
-        tasks2.size
     }
 
     @Test
