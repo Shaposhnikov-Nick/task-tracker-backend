2d575280466eafd7cfb5bfdabd3c7906ea0fc5dd
Shaposhnikov-Nick <shaposhnikov.nick@gmail.com>
2025-03-07 12:40:17 +0300

    [Task] move user functionality from auth-service

---
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/dto/UserDto.kt
@@ -0,0 +1,78 @@
+package ru.tasktracker.taskservice.dto
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import cz.encircled.skom.Convertable
+import jakarta.validation.constraints.*
+import org.springframework.format.annotation.DateTimeFormat
+import ru.tasktracker.taskservice.dto.validation.ValidationGroups
+import java.time.LocalDateTime
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class UserDto(
+
+    @field:Null(groups = [ValidationGroups.Create::class], message = "Id must be null")
+    var id: Long? = null,
+
+    @field:NotBlank(groups = [ValidationGroups.Create::class], message = "Login can't be empty")
+    @field:Size(groups = [ValidationGroups.Create::class], min = 5, max = 50, message = "Login must be between 5 and 50 characters")
+    val login: String? = null,
+
+    @field:NotBlank(groups = [ValidationGroups.Create::class], message = "Password can't be empty")
+    @field:Size(groups = [ValidationGroups.Create::class],min = 7, max = 50, message = "Password must be between 7 and 50 characters")
+    var password: String? = null,
+    val emailConfirmed: Boolean? = null,
+    val blocked: Boolean? = null,
+    val roles: List<RoleDto>? = null,
+    val profile: UserProfileDto,
+    var createdDate: LocalDateTime? = null,
+    var updateDate: LocalDateTime? = null,
+    var updatedBy: String? = null
+) : Convertable
+
+data class RoleDto(
+    val id: Long,
+    val name: String
+) : Convertable
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class UserProfileDto(
+
+    @field:NotBlank(groups = [ValidationGroups.Create::class, ValidationGroups.Update::class], message = "Name can't be empty")
+    val name: String,
+
+    @field:NotBlank(groups = [ValidationGroups.Create::class, ValidationGroups.Update::class], message = "Lastname can't be empty")
+    val lastName: String,
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    val birthday: LocalDateTime,
+
+    @field:Email(
+        groups = [ValidationGroups.Create::class],
+        message = "The string has to be a well-formed email address"
+    )
+    val email: String? = null,
+
+    val about: String? = null,
+
+    val avatar: ByteArray? = null
+) : Convertable
+
+data class PaginationParams(
+
+    @field:Min(1)
+    val pageNumber: Int = 1,
+
+    @field:Min(1)
+    val pageSize: Int = 1
+)
+
+data class UsersPageResponse(
+    val content: List<UserDto>,
+    val page: Int,
+    val totalPages: Int,
+    val totalItems: Long
+)
---
+++ task-tracker-service/src/main/kotlin/ru/tasktracker/taskservice/entity/UserEntity.kt
@@ -0,0 +1,99 @@
+package ru.tasktracker.taskservice.entity
+
+import cz.encircled.skom.Convertable
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+
+@Entity
+@Table(name = "`Users`")
+class User(
+
+    @Column
+    val login: String,
+
+    @Column
+    val password: String,
+
+    @Column(name = "email_confirmed")
+    var emailConfirmed: Boolean,
+
+    @Column
+    val blocked: Boolean,
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "`UserRole`",
+        joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
+    )
+    val roles: MutableSet<Role> = mutableSetOf(),
+
+    @OneToOne(cascade = [CascadeType.ALL])
+    @JoinColumn(name = "profile_id", referencedColumnName = "id")
+    var profile: UserProfile,
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = [CascadeType.ALL], orphanRemoval = true)
+    val tasks: MutableSet<Task> = mutableSetOf()
+
+) : BaseEntity(), Convertable {
+
+    fun addRole(role: Role) {
+        roles.add(role)
+        role.users.add(this)
+    }
+
+    fun removeRole(role: Role) {
+        roles.remove(role)
+        role.users.remove(this)
+    }
+
+    fun addTask(task: Task) {
+        tasks.add(task)
+        task.user = this
+    }
+
+    fun removeTask(task: Task) {
+        tasks.remove(task)
+        task.user = null
+    }
+}
+
+@Entity
+@Table(name = "`UserProfile`")
+class UserProfile(
+
+    @OneToOne(mappedBy = "profile")
+    val user: User? = null,
+
+    @Column
+    var name: String,
+
+    @Column(name = "last_name")
+    var lastName: String,
+
+    @Column
+    var birthday: LocalDateTime,
+
+    @Column
+    var email: String,
+
+    @Column
+    var about: String?,
+
+    @Lob
+    @Column
+    val avatar: ByteArray?
+
+) : BaseEntity(), Convertable
+
+@Entity
+@Table(name = "`Role`")
+class Role(
+
+    val name: String,
+
+    @ManyToMany(mappedBy = "roles")
+    val users: MutableSet<User> = mutableSetOf()
+
+) : BaseEntity(), Convertable
